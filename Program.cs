16491e0123e4bfdde25bf2f47ada0b15d66730c9
Yıldız Nur İbrahim <mehmetyildiznur8@gmail.com>
2024-11-02 00:06:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_14_Ornek4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ornek4();
+            Console.ReadLine();
+        }
+
+        private static void Ornek4()
+        {
+            //Kullanıcıdan bir sayı isteyin
+            //Sayının faktoriyelini ekrana yazdırın
+
+            Console.WriteLine("Lutfen bir sayi giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            int faktoriyel = 1;
+            for (int i =1; i <=sayi; i++)
+            {
+                faktoriyel = faktoriyel * i;
+            }
+            Console.WriteLine("Faktoriyel değeri "+faktoriyel);
+        }
+    }
+}
